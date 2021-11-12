78005d3f06f7f73f232f29c0a7c29b77821b96e3
Olyno <olyno.dev@gmail.com>
2021-11-12 03:37:24 +0100

    chore(auth): remove unecessary fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,25 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import {
-  IsMobilePhone,
-  IsOptional,
-  IsString,
-  MinLength,
-} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsOptional()
-  @IsString()
-  @MinLength(3)
-  readonly first_name?: string;
-
-  @IsOptional()
-  @IsString()
-  @MinLength(3)
-  readonly last_name?: string;
-
-  @IsOptional()
-  @IsMobilePhone()
-  @IsString()
-  readonly phone_number?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
