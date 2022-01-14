779f7ebd077bbffb329ae3337a82eddafd412bb4
Olyno <olyno.dev@gmail.com>
2022-01-15 00:06:31 +0100

    chore: moves interceptor to auth module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PasswordInterceptor } from './users/interceptors/password.interceptor';
+import { PasswordInterceptor } from './auth/interceptors/password.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
